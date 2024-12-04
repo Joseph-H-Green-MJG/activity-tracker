7229f9dbe710b3c278b177a8a73eb482c5a13479
Joseph Green <thecaliman@gmail.com>
2024-12-03 21:18:06 -0800

    Configured GitHub Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'activity-tracker',
   server: {
     open: true,
     port: 8008
